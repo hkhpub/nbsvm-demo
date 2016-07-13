51f82b1cbd493cc9b3e8788108396ee56dc4a13f
hkh <rhkdgh412@gmail.com>
2016-07-14 01:37:19 +0900

    sync

--- nbsvm_run/nbsvm_run.py
+++ nbsvm_run/nbsvm_run.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 from collections import Counter
 
+
 def tokenize(sentence, grams):
     words = sentence.split()
     tokens = []
@@ -12,11 +13,13 @@ def tokenize(sentence, grams):
             tokens += ["_*_".join(words[i:i+gram])]
     return tokens
 
+
 def build_dict(f, grams):
     dic = Counter()
     for sentence in open(f).xreadlines():
         dic.update(tokenize(sentence, grams))
     return dic
+
 
 def process_files(file_pos, file_neg, dic, r, outfn, grams):
     output = []
@@ -40,33 +43,38 @@ def process_files(file_pos, file_neg, dic, r, outfn, grams):
     f.writelines(output)
     f.close()
 
-def compute_ratio(poscounts, negcounts, alpha=1):
-    alltokens = list(set(poscounts.keys() + negcounts.keys()))
+
+def compute_ratio(pos_counter, neg_counter, alpha=1):
+    alltokens = list(set(pos_counter.keys() + neg_counter.keys()))
     dic = dict((t, i) for i, t in enumerate(alltokens))
     d = len(dic)
     print "computing r..."
-    p, q = np.ones(d) * alpha , np.ones(d) * alpha
+    p, q = np.ones(d) * alpha, np.ones(d) * alpha
     for t in alltokens:
-        p[dic[t]] += poscounts[t]
-        q[dic[t]] += negcounts[t]
+        p[dic[t]] += pos_counter[t]
+        q[dic[t]] += neg_counter[t]
     p /= abs(p).sum()
     q /= abs(q).sum()
     r = np.log(p/q)
     return dic, r
- 
+
+
 def main(ptrain, ntrain, ptest, ntest, out, liblinear, ngram):
     ngram = [int(i) for i in ngram]
     print "counting..."
-    poscounts = build_dict(ptrain, ngram)         
-    negcounts = build_dict(ntrain, ngram)         
+    # pos_counter = build_dict(ptrain, ngram)
+    # neg_counter = build_dict(ntrain, ngram)
+    #
+    # dic, r = compute_ratio(pos_counter, neg_counter)
+    # print "processing files..."
+    # process_files(ptrain, ntrain, dic, r, "train-nbsvm.txt", ngram)
+    # process_files(ptest, ntest, dic, r, "test-nbsvm.txt", ngram)
     
-    dic, r = compute_ratio(poscounts, negcounts)
-    print "processing files..."
-    process_files(ptrain, ntrain, dic, r, "train-nbsvm.txt", ngram)
-    process_files(ptest, ntest, dic, r, "test-nbsvm.txt", ngram)
-    
-    trainsvm = os.path.join(liblinear, "train") 
-    predictsvm = os.path.join(liblinear, "predict") 
+    trainsvm = os.path.join(liblinear, "train")
+    predictsvm = os.path.join(liblinear, "predict")
+
+    print trainsvm
+    print predictsvm
     os.system(trainsvm + " -s 0 train-nbsvm.txt model.logreg")
     os.system(predictsvm + " -b 1 test-nbsvm.txt model.logreg " + out)
     # os.system("rm model.logreg train-nbsvm.txt test-nbsvm.txt")
@@ -82,11 +90,11 @@ if __name__ == "__main__":
         --ntest /PATH/data/test-neg.txt\
          --ngram 123 --out TEST-SCORE
     """
-    ptrain = '/home/hkh/sources/dataset/devset/ptrain.txt'
-    ntrain = '/home/hkh/sources/dataset/devset/ntrain.txt'
-    ptest = '/home/hkh/sources/dataset/devset/ptest.txt'
-    ntest = '/home/hkh/sources/dataset/devset/ntest.txt'
-    liblinear = 'liblinear-1.96'
+    ptrain = 'D:/sources/nbsvm-demo/nbsvm_run/data/train-pos.txt'
+    ntrain = 'D:/sources/nbsvm-demo/nbsvm_run/data/train-neg.txt'
+    ptest = 'D:/sources/nbsvm-demo/nbsvm_run/data/test-pos.txt'
+    ntest = 'D:/sources/nbsvm-demo/nbsvm_run/data/test-neg.txt'
+    liblinear = 'D:/sources/nbsvm-demo/nbsvm_run/liblinear-1.96/windows'
     out = 'TEST-SCORE'
 
     main(ptrain, ntrain, ptest, ntest, out, liblinear, '12')
